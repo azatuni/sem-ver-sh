b8da983698291590107500dfe05fa719c4b5546f
Areg Hakobyan <azatuni@gmail.com>
2019-11-20 11:48:19 +0400

    refactor: optimize semver.sh

--- semver.sh
+++ semver.sh
@@ -2,19 +2,41 @@
 #Version: 0.0.8
 #Author: https://github.com/azatuni/sem-ver-sh
 
+
+function set_variables () {
+#Git commit title patterns
 MAJOR_VERSION_PATTERN="BREAKING CHANGE"
 MINOR_VERSION_PATTERN="feat:"
 PATCH_VERSION_PATTERN="fix: docs: style: refactor: perf: test: chore:"
+#Get version
+SEMVERSH_VERSION=`head $0| grep Version| cut -d ' ' -f2`
+#Echo colours
+RED='\e[31m'
+GREEN='\e[1;32m'
+BLUE='\e[1;34m'
+NORMAL=$(tput sgr0)
+BOLD=$(tput bold)
+INFO_STATUS="${YELLOW}${BOLD}[ INFO ]${NORMAL}"
+FATAL_ERROR="${RED}${BOLD}[ FATAL_ERROR! ]${NORMAL}"
+OK_STATUS="${GREEN}${BOLD}[ OK ]${NORMAL}"
+FAIL_STATUS="${RED}${BOLD}[ FAILED ]${NORMAL}"
+DONE_STATUS="${GREEN}${BOLD}[ DONE ]${NORMAL}"
+}
 
 function semversh_help () {
-#help dzmrtel u normal gunazardel errorner u messagner
-echo -e "
-Usage: $0
+echo -e "${BLUE}                                       _     
+                                      | |    
+  ___  ___ _ __ _____   _____ _ __ ___| |__  
+ / __|/ _ \ '_ \` _ \ \ / / _ \ '__/ __| '_ \ 
+ \__ \  __/ | | | | \ V /  __/ |_ \__ \ | | |
+ |___/\\___|_| |_| |_|\\_/ \\___|_(_)|___/_| |_|
+\t\tVERSION: $SEMVERSH_VERSION${NORMAL}"
+echo -e "Usage: $0
 \t-h|--help\t\t\tshow this help and exit from script
-\t[ -b BRANCH_NAME ]\t\tanalyze commit log from specific git branch. It is not recommendated to use this parameter. Default branch: master
-\t[ -f VERSION_FILE ]\t\tdefine file with package version in which new tag will be set near version line
-\t[ --changelog CHANGELOG_FILE ]\twill write change log exept git tag also in file. If CHANGELOG_FILE is not defiend CHANGELOG.md will be used
-\t--dry-run\t\t\trun script in the dry run mode (no any updates pushed)
+\t[ -b BRANCH_NAME ]\t\tanalyze commit log from specific git branch. It is not recommendated to use this parameter. Default branch: 'master'
+\t[ -f VERSION_FILE ]\t\tdefine file with package version in which new tag will be set as version in 'version' line
+\t[ --changelog CHANGELOG_FILE ]\twrite changelog also in file. If CHANGELOG_FILE is not defined 'CHANGELOG.md' will be used
+\t--dry-run\t\t\trun $0 in the dry run mode (no any updates pushed)
 "
 }
 
@@ -72,14 +94,14 @@ case $1 in
 		NEW_VERSION="$CURRENT_MAJOR_VERSION.$CURRENT_MINOR_VERSION.$CURRENT_PATCH_VERSION"
 	;;
 esac
-echo -e "New version set to $NEW_VERSION"
+echo -e "New version set to ${GREEN}$NEW_VERSION${NORMAL}"
 }
 
 function check_git_health () {
 if ! git status > /dev/null
-	then	echo -e "`pwd` is not git directory" && exit 2
+	then	echo -e "${RED}`pwd` is not git directory\t${FATAL_ERROR}" && exit 2
 elif ! git log --pretty=oneline > /dev/null
-	then	echo "No old commits logs found" && exit 3
+	then	echo -e "${RED}No old commits logs found\t${FATAL_ERROR}" && exit 3
 fi
 }
 
@@ -88,7 +110,7 @@ if git describe --tags --abbrev=0 &> /dev/null
 	then	LATEST_GIT_TAG=`git describe --tags --abbrev=0`
 		LATEST_VERSION=`echo $LATEST_GIT_TAG | sed s/v//`
 		LATEST_VERSION_COMMIT=`git rev-list -n 1 $LATEST_GIT_TAG`
-		echo -e "Last release tag is $LATEST_GIT_TAG with $LATEST_VERSION_COMMIT commit hash"
+		echo -e "Last release tag is ${GREEN}$LATEST_GIT_TAG${NORMAL} with ${GREEN}$LATEST_VERSION_COMMIT${NORMAL} commit hash"
 	else	echo -e "No old git tags was found"
 		LATEST_VERSION=0.0.0
 fi
@@ -99,7 +121,7 @@ analyze_version major && test ! -z $NEW_VERSION && return
 analyze_version minor && test ! -z $NEW_VERSION && return
 analyze_version patch && test ! -z $NEW_VERSION && return
 if [ -z "$NEW_VERSION" ] 
-        then    echo -e "No new version was set. Possibly wrong pattern/patterns for commit titles" && exit 4
+        then    echo -e "${RED}No new version was set. Possibly wrong pattern/patterns for commit titles\t${FATAL_ERROR}" && exit 4
 fi
 }
 
@@ -179,16 +201,7 @@ OTHER_COMMIT_HASHES_COUNT=`echo $OTHER_COMMIT_HASHES | wc -w`
 function push_tag () {
 git tag -a v"$NEW_VERSION" -m "
 Release $NEW_VERSION
-`test $BREAKING_CHANGE_COMMIT_HASHES_COUNT != 0 && parse_commit "Breaking_change" "$BREAKING_CHANGE_COMMIT_HASHES"`
-`test $FEATURE_COMMIT_HASHES_COUNT != 0 && parse_commit "Feature" "$FEATURE_COMMIT_HASHES"`
-`test $FIX_COMMIT_HASHES_COUNT != 0 && parse_commit "Fix" "$FIX_COMMIT_HASHES"`
-`test $DOCS_COMMIT_HASHES_COUNT != 0 && parse_commit "Doc" "$DOCS_COMMIT_HASHES"`
-`test $STYLE_COMMIT_HASHES_COUNT != 0 && parse_commit "Style" "$STYLE_COMMIT_HASHES"`
-`test $REFACTOR_COMMIT_HASHES_COUNT != 0 && parse_commit "Refactor" "$REFACTOR_COMMIT_HASHES"`
-`test $PERF_COMMIT_HASHES_COUNT != 0 && parse_commit "Perf" "$PERF_COMMIT_HASHES"`
-`test $TEST_COMMIT_HASHES_COUNT != 0 && parse_commit "Test" "$TEST_COMMIT_HASHES"`
-`test $CHORE_COMMIT_HASHES_COUNT != 0 && parse_commit  "Chore" "$CHORE_COMMIT_HASHES"`
-`test $OTHER_COMMIT_HASHES_COUNT != 0 && parse_commit "Other" "$OTHER_COMMIT_HASHES"`
+`analyze_change_log`
 "
 git push origin v"$NEW_VERSION"
 }
@@ -196,7 +209,7 @@ git push origin v"$NEW_VERSION"
 function generate_changelog_file () {
 echo -e "Release $NEW_VERSION" > $CHANGELOG_FILE && analyze_change_log >> $CHANGELOG_FILE
 if [ $? == 0 ]
-	then	echo "Generated changelog in $CHANGELOG_FILE file"
+	then	echo "Generated changelog in $CHANGELOG_FILE file\t${OK_STATUS}"
 fi
 }
 
@@ -204,15 +217,15 @@ function update_version_file () {
 VERSION_LINE_NUMBER=`cat -n $VERSION_FILE | grep version| awk '{print $1}'`
 sed -i "${VERSION_LINE_NUMBER}s/"$OLD_VERSION_FROM_VERSION_FILE"/"$NEW_VERSION"/" "$VERSION_FILE" 
 if [ $? == 0 ]
-        then    echo -e "Changed version from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE"
-        else    echo -e "Error! Failed to change version from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE" && exit 7
+        then    echo -e "Changed version from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE\t${OK_STATUS}"
+        else    echo -e "${RED}Failed to change version from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE\t${FATAL_ERROR}" && exit 7
 fi
 }
 
 
 
 function commit_and_push_new_version_file () {
-git add "$VERSION_FILE" && git commit -m "version set from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE" && git push origin $GIT_BRANCH
+git add "$VERSION_FILE" && git commit -m "$0: version set from $OLD_VERSION_FROM_VERSION_FILE to $NEW_VERSION in $VERSION_FILE" && git push origin $GIT_BRANCH
 }
 
 function git_add_file () {
@@ -221,7 +234,7 @@ git status | grep -q "$1" && git add "$1"
 
 function try_git_commit_push () {
 GIT_STAGE_FILES=`git diff --name-only --cached`
-test ! -z "$GIT_STAGE_FILES" && git commit -m "Modify $GIT_STAGE_FILE" && git push origin "$GIT_BRANCH"
+test ! -z "$GIT_STAGE_FILES" && git commit -m "$0 modified $GIT_STAGE_FILES" && git push origin "$GIT_BRANCH"
 }
 
 function main_run () {
@@ -244,6 +257,13 @@ else	echo -e "Error!" && exit 1
 fi
 }
 
+function check_version_file () {
+test -z $VERSION_FILE && echo -e "${RED}No version file specified after -f option\t${FATAL_ERROR}" && exit 5
+test ! -f $VERSION_FILE && echo -e "${RED}$VERSION_FILE file doesn't exists\t${FATAL_ERROR}" && exit 6
+OLD_VERSION_FROM_VERSION_FILE=`grep version $VERSION_FILE | grep -Eo "[0-9]{1,}.[0-9]{1,}.[0-9]{1,}"`
+test -z $OLD_VERSION_FROM_VERSION_FILE && echo -e "${RED}No 'version' string was found  or old version was set in $VERSION_FILE." && echo -e "If it is a first release just set version to 0.0.0\t\t${FATAL_ERROR}" && exit 6
+}
+
 
 function pasparam_parser () {
 if [  $# != 0 ]
@@ -251,14 +271,7 @@ if [  $# != 0 ]
 		test $1 == '--help' || test $1 == '-h' && semversh_help && exit 0	
 		echo $@ | grep -q '\-b' && GIT_BRANCH=`echo $@| grep -o "\-b\ [a-zA-Z0-9]\{1,\}"| cut -d " " -f2`
 		echo $@ | grep -q '\-\-dry\-run' && DRY_RUN_MODE="yes" 
-		if echo $@ | grep -q '\-f'
-#es version file manipulyacianner arandzin funkciai mej mtcnel
-                        then    VERSION_FILE=`echo $@| grep -o '\-f\ [a-zA-Z0-9_-.]\{1,\}'| cut -d " " -f2`
-				test -z $VERSION_FILE && echo -e "Error! No version file specified after -f option" && exit 5
-				test ! -f $VERSION_FILE && echo -e "Error! $VERSION_FILE doesn't exists" && exit 6
-				OLD_VERSION_FROM_VERSION_FILE=`grep version $VERSION_FILE | grep -Eo "[0-9]{1,}.[0-9]{1,}.[0-9]{1,}"`
-				test -z $OLD_VERSION_FROM_VERSION_FILE && echo -e "Error! No 'version' string was found  or old version was set in $VERSION_FILE! If it is a first release just set version to 0.0.0" && exit 6
-		fi
+		echo $@ | grep -q '\-f' && VERSION_FILE=`echo $@| grep -o '\-f\ [a-zA-Z0-9_-.]\{1,\}'| cut -d " " -f2` && check_version_file
 		if echo $@ | grep -q '\-\-changelog'
 			then	CHANGELOG_FILE=`echo $@ | grep -o '\-\-changelog\ [a-zA-Z0-9_-.]\{3,\}'| cut -d " " -f2`
 				test -z $CHANGELOG_FILE && CHANGELOG_FILE="CHANGELOG.md"
@@ -268,5 +281,6 @@ test -z "$GIT_BRANCH" && GIT_BRANCH="master"
 test -z "$DRY_RUN_MODE" && DRY_RUN_MODE="no"
 }
 
+set_variables
 pasparam_parser $@
 main_run
